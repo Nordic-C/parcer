817ec753d9b1a53acf895325d3b425f311634aa2
thepigcat <benpospo@gmail.com>
2024-07-16 15:24:03 +0200

    update ast

--- src/ast/expr.rs
+++ src/ast/expr.rs
@@ -13,20 +13,16 @@ pub enum Expression<'ast> {
 
     Ident(Ident<'ast>),
 
-    Deref(&'ast Expression<'ast>),
-    AddrOf(&'ast Expression<'ast>),
-
-    Sizeof(&'ast Expression<'ast>),
-
-    Call(CallExpr<'ast>),
-    BinaryOperation(BinOpExpr<'ast>),
-    UnaryOperation(UnOpExpr<'ast>),
-
+    Infix(InfixExpr<'ast>),
     Prefix(PrefixExpr<'ast>),
 
-    // Assignment
-    CompoundAssignment(CompoundAssignmentExpr<'ast>),
-    Assignment(AssignmentExpr<'ast>),
+    Call(CallExpr<'ast>)
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct CallExpr<'ast> {
+    pub val: &'ast Expression<'ast>,
+    pub args: Vec<Expression<'ast>>,
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -36,55 +32,16 @@ pub struct PrefixExpr<'ast> {
 }
 
 #[derive(Debug, PartialEq, Clone)]
-pub struct CallExpr<'ast> {
-    pub name: Ident<'ast>,
-    pub args: Vec<Expression<'ast>>,
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub struct BinOpExpr<'ast> {
+pub struct InfixExpr<'ast> {
     pub left: &'ast Expression<'ast>,
     pub right: &'ast Expression<'ast>,
-    pub operator: BinOperator,
+    pub op: InOperator,
 }
 
 #[derive(Debug, PartialEq, Clone)]
-pub struct UnOpExpr<'ast> {
-    pub left: &'ast Expression<'ast>,
-    pub operator: UnOperator,
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub struct CompoundAssignmentExpr<'ast> {
-    pub ident: Ident<'ast>,
+pub struct PostExpr<'ast> {
     pub val: &'ast Expression<'ast>,
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub struct AssignmentExpr<'ast> {
-    pub ident: Ident<'ast>,
-    pub val: &'ast Expression<'ast>,
-    pub operator: BinOperator,
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub enum BinOperator {
-    Add,
-    Sub,
-    Mul,
-    Div,
-    Mod,
-    LSh,
-    RSh,
-    BAnd,
-    BOr,
-    BXor,
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub enum UnOperator {
-    Incr,
-    Decr,
+    pub op: PostOperator,
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -98,4 +55,50 @@ pub enum PreOperator<'ast> {
     AddrOf,
     AlignOf,
     Cast(Type<'ast>),
+    Incr,
+    Decr,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum InOperator {
+    // Arithmetic Operators
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    // Bitwise Operators
+    LSh,
+    RSh,
+    BAnd,
+    BOr,
+    BXor,
+    // Relational Operators
+    Eq,
+    Neq,
+    LT,
+    GT,
+    LTE,
+    GTE,
+    // Logical Operators
+    And,
+    Or,
+    // Assignment
+    Assign,
+    AssignAdd,
+    AssignSub,
+    AssignMul,
+    AssignDiv,
+    AssignMod,
+    AssignLsh,
+    AssignRsh,
+    AssingBAnd,
+    AssignBOr,
+    AssignBXor,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum PostOperator {
+    Incr,
+    Decr,
 }
